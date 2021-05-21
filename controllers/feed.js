bc951c24dac8a1777606d8109ba7f076c406f43f
seongjinbrian <seongjinchang8@gmail.com>
2021-05-21 17:00:03 +0900

    Start Testing REST APIs in Node.js

---
+++ controllers/feed.js
@@ -0,0 +1,16 @@
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{title: 'First Post', content: 'This is the first post!' }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    // Create post in db
+    const title = req.body.title;
+    const content = req.body.content;
+
+    res.json({
+        message: 'Post created successfully!',
+        post: {id: new Date().toISOString(), title: title, content: content}
+    });
+};
